stopcontrol/Firmware

PX4 general work queue

src/platforms/common/px4_work_queue/ScheduledWorkItem.hpp
@@ -31,81 +31,51 @@
  *
  ****************************************************************************/
 
-#include "px4_init.h"
+#pragma once
 
-#include <px4_config.h>
-#include <px4_defines.h>
+#include "WorkItem.hpp"
+
 #include <drivers/drv_hrt.h>
-#include <lib/parameters/param.h>
-#include <px4_work_queue/wq_start.h>
-#include <systemlib/cpuload.h>
 
-#include <fcntl.h>
-
-
-#include "platform/cxxinitialize.h"
-
-int px4_platform_init(void)
+namespace px4
 {
 
-#if defined(CONFIG_HAVE_CXX) && defined(CONFIG_HAVE_CXXINITIALIZE)
-	/* run C++ ctors before we go any further */
-	up_cxxinitialize();
+class ScheduledWorkItem : public WorkItem
+{
 
-#	if defined(CONFIG_SYSTEM_NSH_CXXINITIALIZE)
-#  		error CONFIG_SYSTEM_NSH_CXXINITIALIZE Must not be defined! Use CONFIG_HAVE_CXX and CONFIG_HAVE_CXXINITIALIZE.
-#	endif
+public:
 
-#else
-#  error platform is dependent on c++ both CONFIG_HAVE_CXX and CONFIG_HAVE_CXXINITIALIZE must be defined.
-#endif
+	ScheduledWorkItem(const wq_config &config) : WorkItem(config) {}
+	virtual ~ScheduledWorkItem() override;
 
-
-#if !defined(CONFIG_DEV_CONSOLE) && defined(CONFIG_DEV_NULL)
-
-	/* Support running nsh on a board with out a console
-	 * Without this the assumption that the fd 0..2 are
-	 * std{in..err} will be wrong. NSH will read/write to the
-	 * fd it opens for the init script or nested scripts assigned
-	 * to fd 0..2.
+	/**
+	 * Schedule next run with a delay in microseconds.
 	 *
+	 * @param delay_us		The delay in microseconds.
 	 */
+	void ScheduleDelayed(uint32_t delay_us);
 
-	int fd = open("/dev/null", O_RDWR);
+	/**
+	 * Schedule repeating run with optional delay.
+	 *
+	 * @param interval_us		The interval in microseconds.
+	 * @param delay_us			The delay (optional) in microseconds.
+	 */
+	void ScheduleOnInterval(uint32_t interval_us, uint32_t delay_us = 0);
 
-	if (fd == 0) {
-		/* Successfully opened /dev/null as stdin (fd == 0) */
+	/**
+	 * Clear any scheduled work.
+	 */
+	void ScheduleClear();
 
-		(void)fs_dupfd2(0, 1);
-		(void)fs_dupfd2(0, 2);
-		(void)fs_fdopen(0, O_RDONLY,         NULL);
-		(void)fs_fdopen(1, O_WROK | O_CREAT, NULL);
-		(void)fs_fdopen(2, O_WROK | O_CREAT, NULL);
+	virtual void Run() override = 0;
 
-	} else {
-		/* We failed to open /dev/null OR for some reason, we opened
-		 * it and got some file descriptor other than 0.
-		 */
+private:
 
-		if (fd > 0) {
-			(void)close(fd);
-		}
+	static void	schedule_trampoline(void *arg);
 
-		return -ENFILE;
-	}
+	hrt_call	_call{};
 
-#endif
+};
 
-	hrt_init();
-
-	param_init();
-
-	/* configure CPU load estimation */
-#ifdef CONFIG_SCHED_INSTRUMENTATION
-	cpuload_initialize_once();
-#endif
-
-	wq_manager_start();
-
-	return PX4_OK;
-}
+} // namespace px4

abhishek-kumar-prodev/PX4-Autopilot-ZenFCV2

src/platforms/common/px4_work_queue/ScheduledWorkItem.hpp
@@ -31,81 +31,50 @@
  *
  ****************************************************************************/
 
-#include "px4_init.h"
+#pragma once
 
-#include <px4_config.h>
-#include <px4_defines.h>
+#include "WorkItem.hpp"
+
 #include <drivers/drv_hrt.h>
-#include <lib/parameters/param.h>
-#include <px4_work_queue/WorkQueueManager.hpp>
-#include <systemlib/cpuload.h>
 
-#include <fcntl.h>
-
-
-#include "platform/cxxinitialize.h"
-
-int px4_platform_init(void)
+namespace px4
 {
 
-#if defined(CONFIG_HAVE_CXX) && defined(CONFIG_HAVE_CXXINITIALIZE)
-	/* run C++ ctors before we go any further */
-	up_cxxinitialize();
+class ScheduledWorkItem : public WorkItem
+{
+public:
 
-#	if defined(CONFIG_SYSTEM_NSH_CXXINITIALIZE)
-#  		error CONFIG_SYSTEM_NSH_CXXINITIALIZE Must not be defined! Use CONFIG_HAVE_CXX and CONFIG_HAVE_CXXINITIALIZE.
-#	endif
+	ScheduledWorkItem(const wq_config_t &config) : WorkItem(config) {}
+	virtual ~ScheduledWorkItem() override;
 
-#else
-#  error platform is dependent on c++ both CONFIG_HAVE_CXX and CONFIG_HAVE_CXXINITIALIZE must be defined.
-#endif
-
-
-#if !defined(CONFIG_DEV_CONSOLE) && defined(CONFIG_DEV_NULL)
-
-	/* Support running nsh on a board with out a console
-	 * Without this the assumption that the fd 0..2 are
-	 * std{in..err} will be wrong. NSH will read/write to the
-	 * fd it opens for the init script or nested scripts assigned
-	 * to fd 0..2.
+	/**
+	 * Schedule next run with a delay in microseconds.
 	 *
+	 * @param delay_us		The delay in microseconds.
 	 */
+	void ScheduleDelayed(uint32_t delay_us);
 
-	int fd = open("/dev/null", O_RDWR);
+	/**
+	 * Schedule repeating run with optional delay.
+	 *
+	 * @param interval_us		The interval in microseconds.
+	 * @param delay_us			The delay (optional) in microseconds.
+	 */
+	void ScheduleOnInterval(uint32_t interval_us, uint32_t delay_us = 0);
 
-	if (fd == 0) {
-		/* Successfully opened /dev/null as stdin (fd == 0) */
+	/**
+	 * Clear any scheduled work.
+	 */
+	void ScheduleClear();
 
-		(void)fs_dupfd2(0, 1);
-		(void)fs_dupfd2(0, 2);
-		(void)fs_fdopen(0, O_RDONLY,         NULL);
-		(void)fs_fdopen(1, O_WROK | O_CREAT, NULL);
-		(void)fs_fdopen(2, O_WROK | O_CREAT, NULL);
+	virtual void Run() override = 0;
 
-	} else {
-		/* We failed to open /dev/null OR for some reason, we opened
-		 * it and got some file descriptor other than 0.
-		 */
+private:
 
-		if (fd > 0) {
-			(void)close(fd);
-		}
+	static void	schedule_trampoline(void *arg);
 
-		return -ENFILE;
-	}
+	hrt_call	_call{};
 
-#endif
+};
 
-	hrt_init();
-
-	param_init();
-
-	/* configure CPU load estimation */
-#ifdef CONFIG_SCHED_INSTRUMENTATION
-	cpuload_initialize_once();
-#endif
-
-	px4::WorkQueueManagerStart();
-
-	return PX4_OK;
-}
+} // namespace px4