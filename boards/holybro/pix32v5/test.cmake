zeroos/PX4-Autopilot

boards: move testing to dedicated test variants

boards/holybro/pix32v5/test.cmake
@@ -1,33 +1,38 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR holybro
+	MODEL pix32v5
+	LABEL default
 	TOOLCHAIN arm-none-eabi
-	ARCHITECTURE cortex-m4
-	CONSTRAINED_MEMORY
+	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
-		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL3:/dev/ttyS4
+		TEL4:/dev/ttyS3
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		#heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/bosch/bmi055
+		imu/invensense/icm20602
+		imu/invensense/icm20689
 		imu/invensense/icm20948 # required for ak09916 mag
 		irlock
 		lights # all available light drivers
@@ -39,15 +44,18 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
+		px4io
 		rc_input
 		roboclaw
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +88,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
-		#dmesg
+		bl_update
+		dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +125,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/nxp/fmuk66-v3/test.cmake
@@ -3,11 +3,12 @@ px4_add_board(
 	PLATFORM nuttx
 	VENDOR nxp
 	MODEL fmuk66-v3
-	LABEL socketcan
+	LABEL default
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
 	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
@@ -47,7 +48,7 @@ px4_add_board(
 		safety_button
 		telemetry # all available telemetry drivers
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -116,15 +117,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v3/test.cmake
@@ -1,53 +1,60 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v3
+	LABEL default
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
 	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS6
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/l3gd20
+		imu/lsm303d
+		imu/invensense/icm20608g
 		imu/invensense/icm20948 # required for ak09916 mag
+		imu/invensense/mpu6000
+		imu/invensense/mpu9250
 		irlock
 		lights # all available light drivers
-		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
 		optical_flow # all available optical flow drivers
 		osd
 		pca9685
 		pca9685_pwm_out
-		power_monitor/ina226
+		#power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
-		rc_input
+		px4io
 		roboclaw
 		rpm
-		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +87,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
+		bl_update
 		#dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +124,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v4/test.cmake
@@ -1,37 +1,40 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v4
+	LABEL default
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
 	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
-	UAVCAN_INTERFACES 2
+	TESTING
+	UAVCAN_INTERFACES 1
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		WIFI:/dev/ttyS0
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/invensense/icm20602
+		imu/invensense/icm20608g
 		imu/invensense/icm20948 # required for ak09916 mag
+		imu/invensense/mpu9250
 		irlock
 		lights # all available light drivers
-		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
 		optical_flow # all available optical flow drivers
 		osd
@@ -39,6 +42,7 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
 		rc_input
@@ -46,8 +50,9 @@ px4_add_board(
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +85,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
+		bl_update
 		#dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +122,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v5/test.cmake
@@ -1,33 +1,37 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL default
 	TOOLCHAIN arm-none-eabi
-	ARCHITECTURE cortex-m4
-	CONSTRAINED_MEMORY
+	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
-		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		#heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/bosch/bmi055
+		imu/invensense/icm20602
+		imu/invensense/icm20689
 		imu/invensense/icm20948 # required for ak09916 mag
 		irlock
 		lights # all available light drivers
@@ -39,15 +43,18 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
+		px4io
 		rc_input
 		roboclaw
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +87,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
-		#dmesg
+		bl_update
+		dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +124,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v5x/test.cmake
@@ -1,37 +1,42 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL default
 	TOOLCHAIN arm-none-eabi
-	ARCHITECTURE cortex-m4
-	CONSTRAINED_MEMORY
+	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
+	ETHERNET
 	SERIAL_PORTS
-		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS1
+		GPS2:/dev/ttyS7
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/bosch/bmi088
+		imu/invensense/icm20602
 		imu/invensense/icm20948 # required for ak09916 mag
+		imu/invensense/icm42688p
 		irlock
 		lights # all available light drivers
-		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
 		optical_flow # all available optical flow drivers
 		osd
@@ -39,19 +44,21 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
+		px4io
 		rc_input
 		roboclaw
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
-		battery_status
 		camera_feedback
 		commander
 		dataman
@@ -85,11 +92,12 @@ px4_add_board(
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
-		#dmesg
+		bl_update
+		dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -98,6 +106,7 @@ px4_add_board(
 		motor_test
 		mtd
 		nshterm
+		netman
 		param
 		perf
 		pwm

abhishek-kumar-prodev/PX4-Autopilot-ZenFCV2

boards/holybro/pix32v5/test.cmake
@@ -1,33 +1,38 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR holybro
+	MODEL pix32v5
+	LABEL test
 	TOOLCHAIN arm-none-eabi
-	ARCHITECTURE cortex-m4
-	CONSTRAINED_MEMORY
+	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
-		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL3:/dev/ttyS4
+		TEL4:/dev/ttyS3
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		#heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/bosch/bmi055
+		imu/invensense/icm20602
+		imu/invensense/icm20689
 		imu/invensense/icm20948 # required for ak09916 mag
 		irlock
 		lights # all available light drivers
@@ -39,15 +44,18 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
+		px4io
 		rc_input
 		roboclaw
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +88,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
-		#dmesg
+		bl_update
+		dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +125,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/nxp/fmuk66-v3/test.cmake
@@ -3,11 +3,12 @@ px4_add_board(
 	PLATFORM nuttx
 	VENDOR nxp
 	MODEL fmuk66-v3
-	LABEL socketcan
+	LABEL test
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
 	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
@@ -47,7 +48,7 @@ px4_add_board(
 		safety_button
 		telemetry # all available telemetry drivers
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -116,15 +117,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v3/test.cmake
@@ -1,53 +1,60 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v3
+	LABEL test
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
 	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS6
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/l3gd20
+		imu/lsm303d
+		imu/invensense/icm20608g
 		imu/invensense/icm20948 # required for ak09916 mag
+		imu/invensense/mpu6000
+		imu/invensense/mpu9250
 		irlock
 		lights # all available light drivers
-		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
 		optical_flow # all available optical flow drivers
 		osd
 		pca9685
 		pca9685_pwm_out
-		power_monitor/ina226
+		#power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
-		rc_input
+		px4io
 		roboclaw
 		rpm
-		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +87,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
+		bl_update
 		#dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +124,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v4/test.cmake
@@ -1,37 +1,40 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v4
+	LABEL test
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m4
 	CONSTRAINED_MEMORY
 	ROMFSROOT px4fmu_common
-	UAVCAN_INTERFACES 2
+	TESTING
+	UAVCAN_INTERFACES 1
 	SERIAL_PORTS
 		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		WIFI:/dev/ttyS0
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/invensense/icm20602
+		imu/invensense/icm20608g
 		imu/invensense/icm20948 # required for ak09916 mag
+		imu/invensense/mpu9250
 		irlock
 		lights # all available light drivers
-		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
 		optical_flow # all available optical flow drivers
 		osd
@@ -39,6 +42,7 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
 		rc_input
@@ -46,8 +50,9 @@ px4_add_board(
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +85,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
+		bl_update
 		#dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +122,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v5/test.cmake
@@ -1,33 +1,37 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL test
 	TOOLCHAIN arm-none-eabi
-	ARCHITECTURE cortex-m4
-	CONSTRAINED_MEMORY
+	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
 	SERIAL_PORTS
-		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		#heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/bosch/bmi055
+		imu/invensense/icm20602
+		imu/invensense/icm20689
 		imu/invensense/icm20948 # required for ak09916 mag
 		irlock
 		lights # all available light drivers
@@ -39,15 +43,18 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
+		px4io
 		rc_input
 		roboclaw
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
@@ -80,16 +87,17 @@ px4_add_board(
 		sensors
 		sih
 		temperature_compensation
-		#uuv_att_control
-		#uuv_pos_control
+		uuv_att_control
+		uuv_pos_control
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
-		#dmesg
+		bl_update
+		dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -116,15 +124,15 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
-		#fake_magnetometer
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
+		fake_gyro
+		fake_magnetometer
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+		work_item
 	)

boards/px4/fmu-v5x/test.cmake
@@ -1,37 +1,42 @@
 
 px4_add_board(
 	PLATFORM nuttx
-	VENDOR nxp
-	MODEL fmuk66-v3
-	LABEL socketcan
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL test
 	TOOLCHAIN arm-none-eabi
-	ARCHITECTURE cortex-m4
-	CONSTRAINED_MEMORY
+	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
 	UAVCAN_INTERFACES 2
+	ETHERNET
 	SERIAL_PORTS
-		GPS1:/dev/ttyS3
-		TEL1:/dev/ttyS4
-		TEL2:/dev/ttyS1
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS1
+		GPS2:/dev/ttyS7
 	DRIVERS
 		adc/ads1115
 		adc/board_adc
 		barometer # all available barometer drivers
-		barometer/mpl3115a2
 		batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		distance_sensor/srf05 # Specific driver
+		dshot
 		gps
+		heater
 		#imu # all available imu drivers
-		imu/fxas21002c
-		imu/fxos8701cq
+		imu/analog_devices/adis16448
+		imu/bosch/bmi088
+		imu/invensense/icm20602
 		imu/invensense/icm20948 # required for ak09916 mag
+		imu/invensense/icm42688p
 		irlock
 		lights # all available light drivers
-		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
 		optical_flow # all available optical flow drivers
 		osd
@@ -39,19 +44,21 @@ px4_add_board(
 		pca9685_pwm_out
 		power_monitor/ina226
 		#protocol_splitter
+		pwm_input
 		pwm_out_sim
 		pwm_out
+		px4io
 		rc_input
 		roboclaw
 		rpm
 		safety_button
 		telemetry # all available telemetry drivers
+		test_ppm
 		tone_alarm
-		uavcan_v1
+		uavcan
 	MODULES
 		airspeed_selector
 		attitude_estimator_q
-		battery_status
 		camera_feedback
 		commander
 		dataman
@@ -85,11 +92,12 @@ px4_add_board(
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
-		#bl_update
-		#dmesg
+		bl_update
+		dmesg
 		dumpfile
 		esc_calib
-		#gpio
+		gpio
+		hardfault_log
 		i2cdetect
 		led_control
 		mft
@@ -98,6 +106,7 @@ px4_add_board(
 		motor_test
 		mtd
 		nshterm
+		netman
 		param
 		perf
 		pwm