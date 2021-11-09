mechanchan/PX4-Autopilot

commander: force arming is not an option from RC

src/modules/commander/Commander.cpp
@@ -901,10 +901,10 @@ Commander::handle_command(const vehicle_command_s &cmd)
 
 					} else {
 						if (cmd_from_manual_stick) {
-							arming_res = arm(arm_disarm_reason_t::RC_STICK, !forced);
+							arming_res = arm(arm_disarm_reason_t::RC_STICK);
 
 						} else if (cmd_from_manual_switch) {
-							arming_res = arm(arm_disarm_reason_t::RC_SWITCH, !forced);
+							arming_res = arm(arm_disarm_reason_t::RC_SWITCH);
 
 						} else {
 							arming_res = arm(arm_disarm_reason_t::COMMAND_INTERNAL, !forced);

abhishek-kumar-prodev/PX4-Autopilot-ZenFCV2

src/modules/commander/Commander.cpp
@@ -981,10 +981,10 @@ Commander::handle_command(const vehicle_command_s &cmd)
 
 					} else {
 						if (cmd_from_manual_stick) {
-							arming_res = arm(arm_disarm_reason_t::rc_stick, !forced);
+							arming_res = arm(arm_disarm_reason_t::rc_stick);
 
 						} else if (cmd_from_manual_switch) {
-							arming_res = arm(arm_disarm_reason_t::rc_switch, !forced);
+							arming_res = arm(arm_disarm_reason_t::rc_switch);
 
 						} else {
 							arming_res = arm(arm_disarm_reason_t::command_internal, !forced);