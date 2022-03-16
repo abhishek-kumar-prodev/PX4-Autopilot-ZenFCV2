junwoo091400/PX4-Autopilot

PreFlightCheck: refactor sensors preflight checks

src/modules/commander/Arming/PreFlightCheck/checks/accelerometerCheck.cpp
@@ -50,70 +50,71 @@ bool PreFlightCheck::isAccelRequired(const uint8_t instance)
 	uORB::SubscriptionData<sensor_accel_s> accel{ORB_ID(sensor_accel), instance};
 	const uint32_t device_id = static_cast<uint32_t>(accel.get().device_id);
 
+	bool is_used_by_nav = false;
+
 	for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 		uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
 		if (device_id > 0 && estimator_status_sub.get().accel_device_id == device_id) {
-			return true;
+			is_used_by_nav = true;
+			break;
 		}
 	}
 
-	return false;
+	return is_used_by_nav;
 }
 
 bool PreFlightCheck::accelerometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-					const bool optional, const bool report_fail)
+					const bool is_mandatory, bool &report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_accel), instance) == PX4_OK);
-	bool calibration_valid = false;
-	bool valid = true;
+	const bool is_required = is_mandatory || isAccelRequired(instance);
+
+	bool is_valid = false;
+	bool is_calibration_valid = false;
+	bool is_value_valid = false;
 
 	if (exists) {
-
 		uORB::SubscriptionData<sensor_accel_s> accel{ORB_ID(sensor_accel), instance};
 
-		valid = (accel.get().device_id != 0) && (accel.get().timestamp != 0);
-
-		if (!valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Accel %u", instance);
-			}
-		}
+		is_valid = (accel.get().device_id != 0) && (accel.get().timestamp != 0);
 
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
-			calibration_valid = true;
+			is_calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCalibrationIndex("ACC", accel.get().device_id) >= 0);
+			is_calibration_valid = (calibration::FindCalibrationIndex("ACC", accel.get().device_id) >= 0);
 		}
 
-		if (!calibration_valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel %u uncalibrated", instance);
-			}
+		const float accel_magnitude = sqrtf(accel.get().x * accel.get().x
+						    + accel.get().y * accel.get().y
+						    + accel.get().z * accel.get().z);
 
-		} else {
-			const float accel_magnitude = sqrtf(accel.get().x * accel.get().x
-							    + accel.get().y * accel.get().y
-							    + accel.get().z * accel.get().z);
-
-			if (accel_magnitude < 4.0f || accel_magnitude > 15.0f /* m/s^2 */) {
-				if (report_fail) {
-					mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Range, hold still on arming");
-				}
-
-				// this is fatal
-				valid = false;
-			}
-		}
-
-	} else {
-		if (!optional && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Sensor %u missing", instance);
+		if (accel_magnitude > 4.0f && accel_magnitude < 15.0f /* m/s^2 */) {
+			is_value_valid = true;
 		}
 	}
 
-	const bool success = calibration_valid && valid;
+	if (report_fail && is_required) {
+		if (!exists) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Sensor %u missing", instance);
+			report_fail = false;
 
-	return success;
+		} else if (!is_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Accel %u", instance);
+			report_fail = false;
+
+		} else if (!is_calibration_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel %u uncalibrated", instance);
+			report_fail = false;
+
+		} else if (!is_value_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Range, hold still on arming");
+			report_fail = false;
+		}
+	}
+
+	const bool is_sensor_ok = is_valid && is_calibration_valid && is_value_valid;
+
+	return is_sensor_ok || !is_required;
 }

src/modules/commander/Arming/PreFlightCheck/checks/gyroCheck.cpp
@@ -49,54 +49,58 @@ bool PreFlightCheck::isGyroRequired(const uint8_t instance)
 	uORB::SubscriptionData<sensor_gyro_s> gyro{ORB_ID(sensor_gyro), instance};
 	const uint32_t device_id = static_cast<uint32_t>(gyro.get().device_id);
 
+	bool is_used_by_nav = false;
+
 	for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 		uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
 		if (device_id > 0 && estimator_status_sub.get().gyro_device_id == device_id) {
-			return true;
+			is_used_by_nav = true;
+			break;
 		}
 	}
 
-	return false;
+	return is_used_by_nav;
 }
 
 bool PreFlightCheck::gyroCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-			       const bool optional, const bool report_fail)
+			       const bool is_mandatory, bool &report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_gyro), instance) == PX4_OK);
-	bool calibration_valid = false;
-	bool valid = false;
+	const bool is_required = is_mandatory || isGyroRequired(instance);
+
+	bool is_valid = false;
+	bool is_calibration_valid = false;
 
 	if (exists) {
-
 		uORB::SubscriptionData<sensor_gyro_s> gyro{ORB_ID(sensor_gyro), instance};
 
-		valid = (gyro.get().device_id != 0) && (gyro.get().timestamp != 0);
-
-		if (!valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Gyro %u", instance);
-			}
-		}
+		is_valid = (gyro.get().device_id != 0) && (gyro.get().timestamp != 0);
 
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
-			calibration_valid = true;
+			is_calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCalibrationIndex("GYRO", gyro.get().device_id) >= 0);
-		}
-
-		if (!calibration_valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro %u uncalibrated", instance);
-			}
-		}
-
-	} else {
-		if (!optional && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro Sensor %u missing", instance);
+			is_calibration_valid = (calibration::FindCalibrationIndex("GYRO", gyro.get().device_id) >= 0);
 		}
 	}
 
-	return calibration_valid && valid;
+	if (report_fail && is_required) {
+		if (!exists) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro Sensor %u missing", instance);
+			report_fail = false;
+
+		} else if (!is_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Gyro %u", instance);
+			report_fail = false;
+
+		} else if (!is_calibration_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro %u uncalibrated", instance);
+			report_fail = false;
+		}
+	}
+
+	const bool is_sensor_ok = is_calibration_valid && is_valid;
+
+	return is_sensor_ok || !is_required;
 }

src/modules/commander/Arming/PreFlightCheck/checks/magnetometerCheck.cpp
@@ -49,48 +49,40 @@ bool PreFlightCheck::isMagRequired(const uint8_t instance)
 	uORB::SubscriptionData<sensor_mag_s> magnetometer{ORB_ID(sensor_mag), instance};
 	const uint32_t device_id = static_cast<uint32_t>(magnetometer.get().device_id);
 
+	bool is_used_by_nav = false;
+
 	for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 		uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
 		if (device_id > 0 && estimator_status_sub.get().mag_device_id == device_id) {
-			return true;
+			is_used_by_nav = true;;
+			break;
 		}
 	}
 
-	return false;
+	return is_used_by_nav;
 }
 
 bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-				       const bool optional, const bool report_fail)
+				       const bool is_mandatory, bool &report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_mag), instance) == PX4_OK);
-	bool calibration_valid = false;
-	bool valid = false;
+	const bool is_required = is_mandatory || isMagRequired(instance);
+
+	bool is_valid = false;
+	bool is_calibration_valid = false;
 	bool is_mag_fault = false;
 
 	if (exists) {
-
 		uORB::SubscriptionData<sensor_mag_s> magnetometer{ORB_ID(sensor_mag), instance};
 
-		valid = (magnetometer.get().device_id != 0) && (magnetometer.get().timestamp != 0);
-
-		if (!valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Compass %u", instance);
-			}
-		}
+		is_valid = (magnetometer.get().device_id != 0) && (magnetometer.get().timestamp != 0);
 
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
-			calibration_valid = true;
+			is_calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCalibrationIndex("MAG", magnetometer.get().device_id) >= 0);
-		}
-
-		if (!calibration_valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass %u uncalibrated", instance);
-			}
+			is_calibration_valid = (calibration::FindCalibrationIndex("MAG", magnetometer.get().device_id) >= 0);
 		}
 
 		for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
@@ -103,25 +95,35 @@ bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_st
 				}
 			}
 		}
-
-		if (is_mag_fault && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass #%u fault", instance);
-		}
-
-	} else {
-		if (!optional && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass Sensor %u missing", instance);
-		}
 	}
 
-	const bool success = calibration_valid && valid && !is_mag_fault;
+	const bool is_sensor_ok = is_valid && is_calibration_valid && !is_mag_fault;
 
 	if (instance == 0) {
-		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG, exists, !optional, success, status);
+		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG, exists, is_required, is_sensor_ok, status);
 
 	} else if (instance == 1) {
-		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG2, exists, !optional, success, status);
+		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG2, exists, is_required, is_sensor_ok, status);
 	}
 
-	return success;
+	if (report_fail && is_required) {
+		if (!exists) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass Sensor %u missing", instance);
+			report_fail = false;
+
+		} else if (!is_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Compass %u", instance);
+			report_fail = false;
+
+		} else if (!is_calibration_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass %u uncalibrated", instance);
+			report_fail = false;
+
+		} else if (is_mag_fault) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass #%u fault", instance);
+			report_fail = false;
+		}
+	}
+
+	return is_sensor_ok || !is_required;
 }

abhishek-kumar-prodev/PX4-Autopilot-ZenFCV2

src/modules/commander/Arming/PreFlightCheck/checks/accelerometerCheck.cpp
@@ -50,70 +50,71 @@ bool PreFlightCheck::isAccelRequired(const uint8_t instance)
 	uORB::SubscriptionData<sensor_accel_s> accel{ORB_ID(sensor_accel), instance};
 	const uint32_t device_id = static_cast<uint32_t>(accel.get().device_id);
 
+	bool is_used_by_nav = false;
+
 	for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 		uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
 		if (device_id > 0 && estimator_status_sub.get().accel_device_id == device_id) {
-			return true;
+			is_used_by_nav = true;
+			break;
 		}
 	}
 
-	return false;
+	return is_used_by_nav;
 }
 
 bool PreFlightCheck::accelerometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-					const bool optional, const bool report_fail)
+					const bool is_mandatory, bool &report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_accel), instance) == PX4_OK);
-	bool calibration_valid = false;
-	bool valid = true;
+	const bool is_required = is_mandatory || isAccelRequired(instance);
+
+	bool is_valid = false;
+	bool is_calibration_valid = false;
+	bool is_value_valid = false;
 
 	if (exists) {
-
 		uORB::SubscriptionData<sensor_accel_s> accel{ORB_ID(sensor_accel), instance};
 
-		valid = (accel.get().device_id != 0) && (accel.get().timestamp != 0);
-
-		if (!valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Accel %u", instance);
-			}
-		}
+		is_valid = (accel.get().device_id != 0) && (accel.get().timestamp != 0);
 
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
-			calibration_valid = true;
+			is_calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCurrentCalibrationIndex("ACC", accel.get().device_id) >= 0);
+			is_calibration_valid = (calibration::FindCurrentCalibrationIndex("ACC", accel.get().device_id) >= 0);
 		}
 
-		if (!calibration_valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel %u uncalibrated", instance);
-			}
+		const float accel_magnitude = sqrtf(accel.get().x * accel.get().x
+						    + accel.get().y * accel.get().y
+						    + accel.get().z * accel.get().z);
 
-		} else {
-			const float accel_magnitude = sqrtf(accel.get().x * accel.get().x
-							    + accel.get().y * accel.get().y
-							    + accel.get().z * accel.get().z);
-
-			if (accel_magnitude < 4.0f || accel_magnitude > 15.0f /* m/s^2 */) {
-				if (report_fail) {
-					mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Range, hold still on arming");
-				}
-
-				// this is fatal
-				valid = false;
-			}
-		}
-
-	} else {
-		if (!optional && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Sensor %u missing", instance);
+		if (accel_magnitude > 4.0f && accel_magnitude < 15.0f /* m/s^2 */) {
+			is_value_valid = true;
 		}
 	}
 
-	const bool success = calibration_valid && valid;
+	if (report_fail && is_required) {
+		if (!exists) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Sensor %u missing", instance);
+			report_fail = false;
 
-	return success;
+		} else if (!is_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Accel %u", instance);
+			report_fail = false;
+
+		} else if (!is_calibration_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel %u uncalibrated", instance);
+			report_fail = false;
+
+		} else if (!is_value_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Accel Range, hold still on arming");
+			report_fail = false;
+		}
+	}
+
+	const bool is_sensor_ok = is_valid && is_calibration_valid && is_value_valid;
+
+	return is_sensor_ok || !is_required;
 }

src/modules/commander/Arming/PreFlightCheck/checks/gyroCheck.cpp
@@ -49,54 +49,58 @@ bool PreFlightCheck::isGyroRequired(const uint8_t instance)
 	uORB::SubscriptionData<sensor_gyro_s> gyro{ORB_ID(sensor_gyro), instance};
 	const uint32_t device_id = static_cast<uint32_t>(gyro.get().device_id);
 
+	bool is_used_by_nav = false;
+
 	for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 		uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
 		if (device_id > 0 && estimator_status_sub.get().gyro_device_id == device_id) {
-			return true;
+			is_used_by_nav = true;
+			break;
 		}
 	}
 
-	return false;
+	return is_used_by_nav;
 }
 
 bool PreFlightCheck::gyroCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-			       const bool optional, const bool report_fail)
+			       const bool is_mandatory, bool &report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_gyro), instance) == PX4_OK);
-	bool calibration_valid = false;
-	bool valid = false;
+	const bool is_required = is_mandatory || isGyroRequired(instance);
+
+	bool is_valid = false;
+	bool is_calibration_valid = false;
 
 	if (exists) {
-
 		uORB::SubscriptionData<sensor_gyro_s> gyro{ORB_ID(sensor_gyro), instance};
 
-		valid = (gyro.get().device_id != 0) && (gyro.get().timestamp != 0);
-
-		if (!valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Gyro %u", instance);
-			}
-		}
+		is_valid = (gyro.get().device_id != 0) && (gyro.get().timestamp != 0);
 
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
-			calibration_valid = true;
+			is_calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCurrentCalibrationIndex("GYRO", gyro.get().device_id) >= 0);
-		}
-
-		if (!calibration_valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro %u uncalibrated", instance);
-			}
-		}
-
-	} else {
-		if (!optional && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro Sensor %u missing", instance);
+			is_calibration_valid = (calibration::FindCurrentCalibrationIndex("GYRO", gyro.get().device_id) >= 0);
 		}
 	}
 
-	return calibration_valid && valid;
+	if (report_fail && is_required) {
+		if (!exists) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro Sensor %u missing", instance);
+			report_fail = false;
+
+		} else if (!is_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Gyro %u", instance);
+			report_fail = false;
+
+		} else if (!is_calibration_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Gyro %u uncalibrated", instance);
+			report_fail = false;
+		}
+	}
+
+	const bool is_sensor_ok = is_calibration_valid && is_valid;
+
+	return is_sensor_ok || !is_required;
 }

src/modules/commander/Arming/PreFlightCheck/checks/magnetometerCheck.cpp
@@ -49,48 +49,40 @@ bool PreFlightCheck::isMagRequired(const uint8_t instance)
 	uORB::SubscriptionData<sensor_mag_s> magnetometer{ORB_ID(sensor_mag), instance};
 	const uint32_t device_id = static_cast<uint32_t>(magnetometer.get().device_id);
 
+	bool is_used_by_nav = false;
+
 	for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
 		uORB::SubscriptionData<estimator_status_s> estimator_status_sub{ORB_ID(estimator_status), i};
 
 		if (device_id > 0 && estimator_status_sub.get().mag_device_id == device_id) {
-			return true;
+			is_used_by_nav = true;;
+			break;
 		}
 	}
 
-	return false;
+	return is_used_by_nav;
 }
 
 bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
-				       const bool optional, const bool report_fail)
+				       const bool is_mandatory, bool &report_fail)
 {
 	const bool exists = (orb_exists(ORB_ID(sensor_mag), instance) == PX4_OK);
-	bool calibration_valid = false;
-	bool valid = false;
+	const bool is_required = is_mandatory || isMagRequired(instance);
+
+	bool is_valid = false;
+	bool is_calibration_valid = false;
 	bool is_mag_fault = false;
 
 	if (exists) {
-
 		uORB::SubscriptionData<sensor_mag_s> magnetometer{ORB_ID(sensor_mag), instance};
 
-		valid = (magnetometer.get().device_id != 0) && (magnetometer.get().timestamp != 0);
-
-		if (!valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Compass %u", instance);
-			}
-		}
+		is_valid = (magnetometer.get().device_id != 0) && (magnetometer.get().timestamp != 0);
 
 		if (status.hil_state == vehicle_status_s::HIL_STATE_ON) {
-			calibration_valid = true;
+			is_calibration_valid = true;
 
 		} else {
-			calibration_valid = (calibration::FindCurrentCalibrationIndex("MAG", magnetometer.get().device_id) >= 0);
-		}
-
-		if (!calibration_valid) {
-			if (report_fail) {
-				mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass %u uncalibrated", instance);
-			}
+			is_calibration_valid = (calibration::FindCurrentCalibrationIndex("MAG", magnetometer.get().device_id) >= 0);
 		}
 
 		for (uint8_t i = 0; i < ORB_MULTI_MAX_INSTANCES; i++) {
@@ -103,25 +95,35 @@ bool PreFlightCheck::magnetometerCheck(orb_advert_t *mavlink_log_pub, vehicle_st
 				}
 			}
 		}
-
-		if (is_mag_fault && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass #%u fault", instance);
-		}
-
-	} else {
-		if (!optional && report_fail) {
-			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass Sensor %u missing", instance);
-		}
 	}
 
-	const bool success = calibration_valid && valid && !is_mag_fault;
+	const bool is_sensor_ok = is_valid && is_calibration_valid && !is_mag_fault;
 
 	if (instance == 0) {
-		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG, exists, !optional, success, status);
+		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG, exists, is_required, is_sensor_ok, status);
 
 	} else if (instance == 1) {
-		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG2, exists, !optional, success, status);
+		set_health_flags(subsystem_info_s::SUBSYSTEM_TYPE_MAG2, exists, is_required, is_sensor_ok, status);
 	}
 
-	return success;
+	if (report_fail && is_required) {
+		if (!exists) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass Sensor %u missing", instance);
+			report_fail = false;
+
+		} else if (!is_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: no valid data from Compass %u", instance);
+			report_fail = false;
+
+		} else if (!is_calibration_valid) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass %u uncalibrated", instance);
+			report_fail = false;
+
+		} else if (is_mag_fault) {
+			mavlink_log_critical(mavlink_log_pub, "Preflight Fail: Compass #%u fault", instance);
+			report_fail = false;
+		}
+	}
+
+	return is_sensor_ok || !is_required;
 }