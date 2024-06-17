zichen1105/PX4-Autopilot

ekf2: innovation sequence monitoring for all aid sources
 - add new 'innovation_filtered' and 'test_ratio_filtered' fields to
   estimator_aid_source topics

src/modules/ekf2/EKF/aid_sources/external_vision/ev_pos_control.cpp
@@ -126,12 +126,14 @@ void Ekf::controlEvPosFusion(const extVisionSample &ev_sample, const bool common
 	}
 
 #if defined(CONFIG_EKF2_GNSS)
+
 	// increase minimum variance if GPS active (position reference)
 	if (_control_status.flags.gps) {
 		for (int i = 0; i < 2; i++) {
 			pos_cov(i, i) = math::max(pos_cov(i, i), sq(_params.gps_pos_noise));
 		}
 	}
+
 #endif // CONFIG_EKF2_GNSS
 
 	const Vector2f measurement{pos(0), pos(1)};
@@ -155,18 +157,24 @@ void Ekf::controlEvPosFusion(const extVisionSample &ev_sample, const bool common
 		}
 	}
 
-	updateHorizontalPositionAidSrcStatus(ev_sample.time_us,
-					     measurement - _ev_pos_b_est.getBias(),        // observation
-					     measurement_var + _ev_pos_b_est.getBiasVar(), // observation variance
-					     math::max(_params.ev_pos_innov_gate, 1.f),    // innovation gate
-					     aid_src);
+	const Vector2f position = measurement - _ev_pos_b_est.getBias();
+	const Vector2f pos_obs_var = measurement_var + _ev_pos_b_est.getBiasVar();
+
+	updateEstimatorAidStatus(aid_src,
+				 ev_sample.time_us,                                      // sample timestamp
+				 position,                                               // observation
+				 pos_obs_var,                                            // observation variance
+				 Vector2f(_state.pos) - position,                        // innovation
+				 Vector2f(getStateVariance<State::pos>()) + pos_obs_var, // innovation variance
+				 math::max(_params.ev_pos_innov_gate, 1.f));             // innovation gate
 
 	// update the bias estimator before updating the main filter but after
 	// using its current state to compute the vertical position innovation
 	if (measurement_valid && quality_sufficient) {
 		_ev_pos_b_est.setMaxStateNoise(Vector2f(sqrtf(measurement_var(0)), sqrtf(measurement_var(1))));
 		_ev_pos_b_est.setProcessNoiseSpectralDensity(_params.ev_hgt_bias_nsd); // TODO
-		_ev_pos_b_est.fuseBias(measurement - Vector2f(_state.pos.xy()), measurement_var + Vector2f(getStateVariance<State::pos>()));
+		_ev_pos_b_est.fuseBias(measurement - Vector2f(_state.pos.xy()),
+				       measurement_var + Vector2f(getStateVariance<State::pos>()));
 	}
 
 	if (!measurement_valid) {
@@ -297,8 +305,6 @@ void Ekf::updateEvPosFusion(const Vector2f &measurement, const Vector2f &measure
 void Ekf::stopEvPosFusion()
 {
 	if (_control_status.flags.ev_pos) {
-		resetEstimatorAidStatus(_aid_src_ev_pos);
-
 		_control_status.flags.ev_pos = false;
 	}
 }

src/modules/ekf2/EKF/aid_sources/external_vision/ev_yaw_control.cpp
@@ -43,11 +43,22 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 {
 	static constexpr const char *AID_SRC_NAME = "EV yaw";
 
-	resetEstimatorAidStatus(aid_src);
-	aid_src.timestamp_sample = ev_sample.time_us;
-	aid_src.observation = getEulerYaw(ev_sample.quat);
-	aid_src.observation_variance = math::max(ev_sample.orientation_var(2), _params.ev_att_noise, sq(0.01f));
-	aid_src.innovation = wrap_pi(getEulerYaw(_R_to_earth) - aid_src.observation);
+	float obs = getEulerYaw(ev_sample.quat);
+	float obs_var = math::max(ev_sample.orientation_var(2), _params.ev_att_noise, sq(0.01f));
+
+	float innov = wrap_pi(getEulerYaw(_R_to_earth) - obs);
+	float innov_var = 0.f;
+
+	VectorState H_YAW;
+	computeYawInnovVarAndH(obs_var, innov_var, H_YAW);
+
+	updateEstimatorAidStatus(aid_src,
+		ev_sample.time_us,                           // sample timestamp
+		obs,                                         // observation
+		obs_var,                                     // observation variance
+		innov,                                       // innovation
+		innov_var,                                   // innovation variance
+		math::max(_params.heading_innov_gate, 1.f)); // innovation gate
 
 	if (ev_reset) {
 		_control_status.flags.ev_yaw_fault = false;
@@ -65,10 +76,6 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 	    && (ev_sample.pos_frame != PositionFrame::LOCAL_FRAME_NED)
 	   ) {
 		continuing_conditions_passing = false;
-
-		// use delta yaw for innovation logging
-		aid_src.innovation = wrap_pi(wrap_pi(getEulerYaw(_R_to_earth) - _ev_yaw_pred_prev)
-					     - wrap_pi(getEulerYaw(ev_sample.quat) - getEulerYaw(_ev_sample_prev.quat)));
 	}
 
 	const bool starting_conditions_passing = common_starting_conditions_passing
@@ -94,7 +101,7 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 				}
 
 			} else if (quality_sufficient) {
-				fuseYaw(aid_src);
+				fuseYaw(aid_src, H_YAW);
 
 			} else {
 				aid_src.innovation_rejected = true;
@@ -141,18 +148,25 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 			if (ev_sample.pos_frame == PositionFrame::LOCAL_FRAME_NED) {
 
 				if (_control_status.flags.yaw_align) {
-					ECL_INFO("starting %s fusion", AID_SRC_NAME);
+
+					if (fuseYaw(aid_src, H_YAW)) {
+						ECL_INFO("starting %s fusion", AID_SRC_NAME);
+						_information_events.flags.starting_vision_yaw_fusion = true;
+
+						_control_status.flags.ev_yaw = true;
+					}
 
 				} else {
 					// reset yaw to EV and set yaw_align
 					ECL_INFO("starting %s fusion, resetting state", AID_SRC_NAME);
+					_information_events.flags.starting_vision_yaw_fusion = true;
+
 					resetQuatStateYaw(aid_src.observation, aid_src.observation_variance);
 					_control_status.flags.yaw_align = true;
-				}
+					_control_status.flags.ev_yaw = true;
 
-				aid_src.time_last_fuse = _time_delayed_us;
-				_information_events.flags.starting_vision_yaw_fusion = true;
-				_control_status.flags.ev_yaw = true;
+					aid_src.time_last_fuse = _time_delayed_us;
+				}
 
 			} else if (ev_sample.pos_frame == PositionFrame::LOCAL_FRAME_FRD) {
 				// turn on fusion of external vision yaw measurements
@@ -178,7 +192,6 @@ void Ekf::stopEvYawFusion()
 {
 #if defined(CONFIG_EKF2_EXTERNAL_VISION)
 	if (_control_status.flags.ev_yaw) {
-		resetEstimatorAidStatus(_aid_src_ev_yaw);
 
 		_control_status.flags.ev_yaw = false;
 	}

src/modules/ekf2/EKF/aid_sources/fake_pos_control.cpp
@@ -63,14 +63,21 @@ void Ekf::controlFakePosFusion()
 			obs_var(0) = obs_var(1) = sq(0.5f);
 		}
 
+		const Vector2f position(_last_known_pos);
+
 		const float innov_gate = 3.f;
 
-		updateHorizontalPositionAidSrcStatus(_time_delayed_us, Vector2f(_last_known_pos), obs_var, innov_gate, aid_src);
-
+		updateEstimatorAidStatus(aid_src,
+					 _time_delayed_us,
+					 position,                                           // observation
+					 obs_var,                                            // observation variance
+					 Vector2f(_state.pos) - position,                    // innovation
+					 Vector2f(getStateVariance<State::pos>()) + obs_var, // innovation variance
+					 innov_gate);                                        // innovation gate
 
 		const bool continuing_conditions_passing = !isHorizontalAidingActive()
-							   && ((getTiltVariance() > sq(math::radians(3.f))) || _control_status.flags.vehicle_at_rest)
-							   && (!(_params.imu_ctrl & static_cast<int32_t>(ImuCtrl::GravityVector)) || _control_status.flags.vehicle_at_rest);
+				&& ((getTiltVariance() > sq(math::radians(3.f))) || _control_status.flags.vehicle_at_rest)
+				&& (!(_params.imu_ctrl & static_cast<int32_t>(ImuCtrl::GravityVector)) || _control_status.flags.vehicle_at_rest);
 
 		const bool starting_conditions_passing = continuing_conditions_passing
 				&& _horizontal_deadreckon_time_exceeded;
@@ -131,7 +138,5 @@ void Ekf::stopFakePosFusion()
 	if (_control_status.flags.fake_pos) {
 		ECL_INFO("stop fake position fusion");
 		_control_status.flags.fake_pos = false;
-
-		resetEstimatorAidStatus(_aid_src_fake_pos);
 	}
 }

src/modules/ekf2/EKF/aid_sources/gravity/gravity_fusion.cpp
@@ -69,19 +69,13 @@ void Ekf::controlGravityFusion(const imuSample &imu)
 	sym::ComputeGravityXyzInnovVarAndHx(state_vector, P, measurement_var, &innovation_variance, &H);
 
 	// fill estimator aid source status
-	resetEstimatorAidStatus(_aid_src_gravity);
-	_aid_src_gravity.timestamp_sample = imu.time_us;
-	measurement.copyTo(_aid_src_gravity.observation);
-
-	for (auto &var : _aid_src_gravity.observation_variance) {
-		var = measurement_var;
-	}
-
-	innovation.copyTo(_aid_src_gravity.innovation);
-	innovation_variance.copyTo(_aid_src_gravity.innovation_variance);
-
-	float innovation_gate = 0.25f;
-	setEstimatorAidStatusTestRatio(_aid_src_gravity, innovation_gate);
+	updateEstimatorAidStatus(_aid_src_gravity,
+				 imu.time_us,                                                 // sample timestamp
+				 measurement,                                                 // observation
+				 Vector3f{measurement_var, measurement_var, measurement_var}, // observation variance
+				 innovation,                                                  // innovation
+				 innovation_variance,                                         // innovation variance
+				 0.25f);                                                      // innovation gate
 
 	bool fused[3] {false, false, false};
 

src/modules/ekf2/EKF/terrain_estimator/terrain_estimator.cpp
@@ -205,8 +205,6 @@ void Ekf::stopHaglRngFusion()
 		ECL_INFO("stopping HAGL range fusion");
 
 		// reset flags
-		resetEstimatorAidStatus(_aid_src_terrain_range_finder);
-
 		_innov_check_fail_status.flags.reject_hagl = false;
 
 		_hagl_sensor_status.flags.range_finder = false;
@@ -221,30 +219,19 @@ void Ekf::updateHaglRng(estimator_aid_source1d_s &aid_src) const
 	// predict the hagl from the vehicle position and terrain height
 	const float pred_hagl = _terrain_vpos - _state.pos(2);
 
-	// calculate the innovation
-	const float hagl_innov = pred_hagl - meas_hagl;
-
 	// calculate the observation variance adding the variance of the vehicles own height uncertainty
 	const float obs_variance = getRngVar();
 
 	// calculate the innovation variance - limiting it to prevent a badly conditioned fusion
 	const float hagl_innov_var = fmaxf(_terrain_var + obs_variance, obs_variance);
 
-	// perform an innovation consistency check and only fuse data if it passes
-	const float innov_gate = fmaxf(_params.range_innov_gate, 1.0f);
-
-
-	aid_src.timestamp_sample = _time_delayed_us; // TODO
-
-	aid_src.observation = meas_hagl;
-	aid_src.observation_variance = obs_variance;
-
-	aid_src.innovation = hagl_innov;
-	aid_src.innovation_variance = hagl_innov_var;
-
-	setEstimatorAidStatusTestRatio(aid_src, innov_gate);
-
-	aid_src.fused = false;
+	updateEstimatorAidStatus(aid_src,
+		_time_delayed_us,                          // sample timestamp
+		meas_hagl,                                 // observation
+		obs_variance,                              // observation variance
+		pred_hagl - meas_hagl,                     // innovation
+		hagl_innov_var,                            // innovation variance
+		math::max(_params.range_innov_gate, 1.f)); // innovation gate
 }
 
 void Ekf::fuseHaglRng(estimator_aid_source1d_s &aid_src)
@@ -329,7 +316,6 @@ void Ekf::stopHaglFlowFusion()
 		ECL_INFO("stopping HAGL flow fusion");
 
 		_hagl_sensor_status.flags.flow = false;
-		resetEstimatorAidStatus(_aid_src_terrain_optical_flow);
 	}
 }
 
@@ -366,9 +352,6 @@ void Ekf::fuseFlowForTerrain(estimator_aid_source2d_s &flow)
 		_terrain_var = 100.0f;
 		return;
 	}
-
-	// run the innovation consistency check and record result
-	setEstimatorAidStatusTestRatio(flow, math::max(_params.flow_innov_gate, 1.f));
 
 	_innov_check_fail_status.flags.reject_optflow_X = (flow.test_ratio[0] > 1.f);
 	_innov_check_fail_status.flags.reject_optflow_Y = (flow.test_ratio[1] > 1.f);

abhishek-kumar-prodev/PX4-Autopilot-ZenFCV2

src/modules/ekf2/EKF/aid_sources/external_vision/ev_pos_control.cpp
@@ -126,12 +126,14 @@ void Ekf::controlEvPosFusion(const extVisionSample &ev_sample, const bool common
 	}
 
 #if defined(CONFIG_EKF2_GNSS)
+
 	// increase minimum variance if GPS active (position reference)
 	if (_control_status.flags.gps) {
 		for (int i = 0; i < 2; i++) {
 			pos_cov(i, i) = math::max(pos_cov(i, i), sq(_params.gps_pos_noise));
 		}
 	}
+
 #endif // CONFIG_EKF2_GNSS
 
 	const Vector2f measurement{pos(0), pos(1)};
@@ -155,18 +157,24 @@ void Ekf::controlEvPosFusion(const extVisionSample &ev_sample, const bool common
 		}
 	}
 
-	updateHorizontalPositionAidSrcStatus(ev_sample.time_us,
-					     measurement - _ev_pos_b_est.getBias(),        // observation
-					     measurement_var + _ev_pos_b_est.getBiasVar(), // observation variance
-					     math::max(_params.ev_pos_innov_gate, 1.f),    // innovation gate
-					     aid_src);
+	const Vector2f position = measurement - _ev_pos_b_est.getBias();
+	const Vector2f pos_obs_var = measurement_var + _ev_pos_b_est.getBiasVar();
+
+	updateAidSourceStatus(aid_src,
+				 ev_sample.time_us,                                      // sample timestamp
+				 position,                                               // observation
+				 pos_obs_var,                                            // observation variance
+				 Vector2f(_state.pos) - position,                        // innovation
+				 Vector2f(getStateVariance<State::pos>()) + pos_obs_var, // innovation variance
+				 math::max(_params.ev_pos_innov_gate, 1.f));             // innovation gate
 
 	// update the bias estimator before updating the main filter but after
 	// using its current state to compute the vertical position innovation
 	if (measurement_valid && quality_sufficient) {
 		_ev_pos_b_est.setMaxStateNoise(Vector2f(sqrtf(measurement_var(0)), sqrtf(measurement_var(1))));
 		_ev_pos_b_est.setProcessNoiseSpectralDensity(_params.ev_hgt_bias_nsd); // TODO
-		_ev_pos_b_est.fuseBias(measurement - Vector2f(_state.pos.xy()), measurement_var + Vector2f(getStateVariance<State::pos>()));
+		_ev_pos_b_est.fuseBias(measurement - Vector2f(_state.pos.xy()),
+				       measurement_var + Vector2f(getStateVariance<State::pos>()));
 	}
 
 	if (!measurement_valid) {
@@ -297,8 +305,6 @@ void Ekf::updateEvPosFusion(const Vector2f &measurement, const Vector2f &measure
 void Ekf::stopEvPosFusion()
 {
 	if (_control_status.flags.ev_pos) {
-		resetEstimatorAidStatus(_aid_src_ev_pos);
-
 		_control_status.flags.ev_pos = false;
 	}
 }

src/modules/ekf2/EKF/aid_sources/external_vision/ev_yaw_control.cpp
@@ -43,11 +43,22 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 {
 	static constexpr const char *AID_SRC_NAME = "EV yaw";
 
-	resetEstimatorAidStatus(aid_src);
-	aid_src.timestamp_sample = ev_sample.time_us;
-	aid_src.observation = getEulerYaw(ev_sample.quat);
-	aid_src.observation_variance = math::max(ev_sample.orientation_var(2), _params.ev_att_noise, sq(0.01f));
-	aid_src.innovation = wrap_pi(getEulerYaw(_R_to_earth) - aid_src.observation);
+	float obs = getEulerYaw(ev_sample.quat);
+	float obs_var = math::max(ev_sample.orientation_var(2), _params.ev_att_noise, sq(0.01f));
+
+	float innov = wrap_pi(getEulerYaw(_R_to_earth) - obs);
+	float innov_var = 0.f;
+
+	VectorState H_YAW;
+	computeYawInnovVarAndH(obs_var, innov_var, H_YAW);
+
+	updateAidSourceStatus(aid_src,
+		ev_sample.time_us,                           // sample timestamp
+		obs,                                         // observation
+		obs_var,                                     // observation variance
+		innov,                                       // innovation
+		innov_var,                                   // innovation variance
+		math::max(_params.heading_innov_gate, 1.f)); // innovation gate
 
 	if (ev_reset) {
 		_control_status.flags.ev_yaw_fault = false;
@@ -65,10 +76,6 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 	    && (ev_sample.pos_frame != PositionFrame::LOCAL_FRAME_NED)
 	   ) {
 		continuing_conditions_passing = false;
-
-		// use delta yaw for innovation logging
-		aid_src.innovation = wrap_pi(wrap_pi(getEulerYaw(_R_to_earth) - _ev_yaw_pred_prev)
-					     - wrap_pi(getEulerYaw(ev_sample.quat) - getEulerYaw(_ev_sample_prev.quat)));
 	}
 
 	const bool starting_conditions_passing = common_starting_conditions_passing
@@ -94,7 +101,7 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 				}
 
 			} else if (quality_sufficient) {
-				fuseYaw(aid_src);
+				fuseYaw(aid_src, H_YAW);
 
 			} else {
 				aid_src.innovation_rejected = true;
@@ -141,18 +148,25 @@ void Ekf::controlEvYawFusion(const extVisionSample &ev_sample, const bool common
 			if (ev_sample.pos_frame == PositionFrame::LOCAL_FRAME_NED) {
 
 				if (_control_status.flags.yaw_align) {
-					ECL_INFO("starting %s fusion", AID_SRC_NAME);
+
+					if (fuseYaw(aid_src, H_YAW)) {
+						ECL_INFO("starting %s fusion", AID_SRC_NAME);
+						_information_events.flags.starting_vision_yaw_fusion = true;
+
+						_control_status.flags.ev_yaw = true;
+					}
 
 				} else {
 					// reset yaw to EV and set yaw_align
 					ECL_INFO("starting %s fusion, resetting state", AID_SRC_NAME);
+					_information_events.flags.starting_vision_yaw_fusion = true;
+
 					resetQuatStateYaw(aid_src.observation, aid_src.observation_variance);
 					_control_status.flags.yaw_align = true;
-				}
+					_control_status.flags.ev_yaw = true;
 
-				aid_src.time_last_fuse = _time_delayed_us;
-				_information_events.flags.starting_vision_yaw_fusion = true;
-				_control_status.flags.ev_yaw = true;
+					aid_src.time_last_fuse = _time_delayed_us;
+				}
 
 			} else if (ev_sample.pos_frame == PositionFrame::LOCAL_FRAME_FRD) {
 				// turn on fusion of external vision yaw measurements
@@ -178,7 +192,6 @@ void Ekf::stopEvYawFusion()
 {
 #if defined(CONFIG_EKF2_EXTERNAL_VISION)
 	if (_control_status.flags.ev_yaw) {
-		resetEstimatorAidStatus(_aid_src_ev_yaw);
 
 		_control_status.flags.ev_yaw = false;
 	}

src/modules/ekf2/EKF/aid_sources/fake_pos_control.cpp
@@ -63,14 +63,21 @@ void Ekf::controlFakePosFusion()
 			obs_var(0) = obs_var(1) = sq(0.5f);
 		}
 
+		const Vector2f position(_last_known_pos);
+
 		const float innov_gate = 3.f;
 
-		updateHorizontalPositionAidSrcStatus(_time_delayed_us, Vector2f(_last_known_pos), obs_var, innov_gate, aid_src);
-
+		updateAidSourceStatus(aid_src,
+					 _time_delayed_us,
+					 position,                                           // observation
+					 obs_var,                                            // observation variance
+					 Vector2f(_state.pos) - position,                    // innovation
+					 Vector2f(getStateVariance<State::pos>()) + obs_var, // innovation variance
+					 innov_gate);                                        // innovation gate
 
 		const bool continuing_conditions_passing = !isHorizontalAidingActive()
-							   && ((getTiltVariance() > sq(math::radians(3.f))) || _control_status.flags.vehicle_at_rest)
-							   && (!(_params.imu_ctrl & static_cast<int32_t>(ImuCtrl::GravityVector)) || _control_status.flags.vehicle_at_rest);
+				&& ((getTiltVariance() > sq(math::radians(3.f))) || _control_status.flags.vehicle_at_rest)
+				&& (!(_params.imu_ctrl & static_cast<int32_t>(ImuCtrl::GravityVector)) || _control_status.flags.vehicle_at_rest);
 
 		const bool starting_conditions_passing = continuing_conditions_passing
 				&& _horizontal_deadreckon_time_exceeded;
@@ -131,7 +138,5 @@ void Ekf::stopFakePosFusion()
 	if (_control_status.flags.fake_pos) {
 		ECL_INFO("stop fake position fusion");
 		_control_status.flags.fake_pos = false;
-
-		resetEstimatorAidStatus(_aid_src_fake_pos);
 	}
 }

src/modules/ekf2/EKF/aid_sources/gravity/gravity_fusion.cpp
@@ -69,19 +69,13 @@ void Ekf::controlGravityFusion(const imuSample &imu)
 	sym::ComputeGravityXyzInnovVarAndHx(state_vector, P, measurement_var, &innovation_variance, &H);
 
 	// fill estimator aid source status
-	resetEstimatorAidStatus(_aid_src_gravity);
-	_aid_src_gravity.timestamp_sample = imu.time_us;
-	measurement.copyTo(_aid_src_gravity.observation);
-
-	for (auto &var : _aid_src_gravity.observation_variance) {
-		var = measurement_var;
-	}
-
-	innovation.copyTo(_aid_src_gravity.innovation);
-	innovation_variance.copyTo(_aid_src_gravity.innovation_variance);
-
-	float innovation_gate = 0.25f;
-	setEstimatorAidStatusTestRatio(_aid_src_gravity, innovation_gate);
+	updateAidSourceStatus(_aid_src_gravity,
+				 imu.time_us,                                                 // sample timestamp
+				 measurement,                                                 // observation
+				 Vector3f{measurement_var, measurement_var, measurement_var}, // observation variance
+				 innovation,                                                  // innovation
+				 innovation_variance,                                         // innovation variance
+				 0.25f);                                                      // innovation gate
 
 	bool fused[3] {false, false, false};
 

src/modules/ekf2/EKF/terrain_estimator/terrain_estimator.cpp
@@ -205,8 +205,6 @@ void Ekf::stopHaglRngFusion()
 		ECL_INFO("stopping HAGL range fusion");
 
 		// reset flags
-		resetEstimatorAidStatus(_aid_src_terrain_range_finder);
-
 		_innov_check_fail_status.flags.reject_hagl = false;
 
 		_hagl_sensor_status.flags.range_finder = false;
@@ -221,30 +219,19 @@ void Ekf::updateHaglRng(estimator_aid_source1d_s &aid_src) const
 	// predict the hagl from the vehicle position and terrain height
 	const float pred_hagl = _terrain_vpos - _state.pos(2);
 
-	// calculate the innovation
-	const float hagl_innov = pred_hagl - meas_hagl;
-
 	// calculate the observation variance adding the variance of the vehicles own height uncertainty
 	const float obs_variance = getRngVar();
 
 	// calculate the innovation variance - limiting it to prevent a badly conditioned fusion
 	const float hagl_innov_var = fmaxf(_terrain_var + obs_variance, obs_variance);
 
-	// perform an innovation consistency check and only fuse data if it passes
-	const float innov_gate = fmaxf(_params.range_innov_gate, 1.0f);
-
-
-	aid_src.timestamp_sample = _time_delayed_us; // TODO
-
-	aid_src.observation = meas_hagl;
-	aid_src.observation_variance = obs_variance;
-
-	aid_src.innovation = hagl_innov;
-	aid_src.innovation_variance = hagl_innov_var;
-
-	setEstimatorAidStatusTestRatio(aid_src, innov_gate);
-
-	aid_src.fused = false;
+	updateAidSourceStatus(aid_src,
+		_time_delayed_us,                          // sample timestamp
+		meas_hagl,                                 // observation
+		obs_variance,                              // observation variance
+		pred_hagl - meas_hagl,                     // innovation
+		hagl_innov_var,                            // innovation variance
+		math::max(_params.range_innov_gate, 1.f)); // innovation gate
 }
 
 void Ekf::fuseHaglRng(estimator_aid_source1d_s &aid_src)
@@ -329,7 +316,6 @@ void Ekf::stopHaglFlowFusion()
 		ECL_INFO("stopping HAGL flow fusion");
 
 		_hagl_sensor_status.flags.flow = false;
-		resetEstimatorAidStatus(_aid_src_terrain_optical_flow);
 	}
 }
 
@@ -366,9 +352,6 @@ void Ekf::fuseFlowForTerrain(estimator_aid_source2d_s &flow)
 		_terrain_var = 100.0f;
 		return;
 	}
-
-	// run the innovation consistency check and record result
-	setEstimatorAidStatusTestRatio(flow, math::max(_params.flow_innov_gate, 1.f));
 
 	_innov_check_fail_status.flags.reject_optflow_X = (flow.test_ratio[0] > 1.f);
 	_innov_check_fail_status.flags.reject_optflow_Y = (flow.test_ratio[1] > 1.f);